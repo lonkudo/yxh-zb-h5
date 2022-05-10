7bff3ba14b55ad284413843d5d50ab9141dca0c3
李傲宇 <648918365@qq.com>
2022-05-10 11:49:26 +0800

    添加搜索

---
+++ pages/search/search.vue
@@ -0,0 +1,20 @@
+<template>
+	<view class="flex">
+		<text>ok</text>
+		<search-page-header></search-page-header>
+	</view>
+</template>
+
+<script>
+	import '@/components/SearchPageHeader/SearchPageHeader.vue'
+	export default {
+		components: {
+			SearchPageHeader,
+		},
+		data() {
+			return {}
+		},
+	}
+</script>
+
+<style lang="scss"></style>

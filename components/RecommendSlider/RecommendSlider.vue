5a2891bb47bc991724876f8418337940268e1cf0
李傲宇 <648918365@qq.com>
2022-05-11 15:17:15 +0800

    暂存

--- components/RecommendSlider/RecommendSlider.vue
+++ components/RecommendSlider/RecommendSlider.vue
@@ -1,34 +1,32 @@
 <template>
-		<view class="slider b-f margin-xs">
-			    <u-swiper
-			            :list="list1"
-			            @change="change"
-			            @click="click"
-						indicator
-			    ></u-swiper>
-		</view>
-
+	<view class="slider b-f margin-xs">
+		<u-swiper
+			:list="list1"
+			@change="change"
+			@click="click"
+			indicator
+			indicatorMode="dot"
+		></u-swiper>
+	</view>
 </template>
 
 <script>
 	export default {
-		name:"RecommendSlider",
+		name: 'RecommendSlider',
 		data() {
 			return {
 				list1: [
-				    'https://cdn.uviewui.com/uview/swiper/swiper1.png',
-				    'https://cdn.uviewui.com/uview/swiper/swiper2.png',
-				    'https://cdn.uviewui.com/uview/swiper/swiper3.png',
+					'https://cdn.uviewui.com/uview/swiper/swiper1.png',
+					'https://cdn.uviewui.com/uview/swiper/swiper2.png',
+					'https://cdn.uviewui.com/uview/swiper/swiper3.png',
 				],
-			};
+			}
 		},
 		methods: {
-			change(){},
-			click(){},
-		}
+			change() {},
+			click() {},
+		},
 	}
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
--- components/SystemInformsSlider/SystemInformsSlider.vue
+++ components/SystemInformsSlider/SystemInformsSlider.vue
@@ -1,18 +1,27 @@
 <template>
-		<view class="sys-informs margin-xs b-f  align-center padding-sm flex br-5">
-			<text class="iconfont icon-laba margin-right-sm fc-g
-			"></text>
-			<swiper class="informs-slider" :indicator-dots="false" :autoplay="true" :interval="10000" :duration="10000" vertical="true" :circular="true">
-				<swiper-item v-for="(item,index) in list2" :key="index">
-					<view class="swiper-item flex align-center"><text class="fc-b-9 fs-24 f-hide-2">{{item}}</text></view>
-				</swiper-item>                                    
-			</swiper>
-		</view>
+	<view class="sys-informs margin-xs b-f align-center padding-sm flex br-5">
+		<text class="iconfont icon-laba margin-right-sm fc-g"></text>
+		<swiper
+			class="informs-slider"
+			:indicator-dots="false"
+			:autoplay="true"
+			:interval="5000"
+			:duration="1000"
+			vertical="true"
+			:circular="true"
+		>
+			<swiper-item v-for="(item, index) in list2" :key="index">
+				<view class="swiper-item flex align-center"
+					><text class="fc-b-9 fs-24 f-hide-2">{{ item }}</text></view
+				>
+			</swiper-item>
+		</swiper>
+	</view>
 </template>
 
 <script>
 	export default {
-		name:"SystemInformsSlider",
+		name: 'SystemInformsSlider',
 		data() {
 			return {
 				list2: [
@@ -22,15 +31,14 @@
 					'wo shi cai ji',
 					'giao',
 				],
-			};
-		}
+			}
+		},
 	}
 </script>
 
 <style lang="scss">
 	.sys-informs {
 		height: 100rpx;
-		
 	}
 	.informs-slider {
 		height: 80rpx;
@@ -42,7 +50,5 @@
 		text {
 			padding-bottom: 3rpx; // 英文下面会有点显示不出。加个padding
 		}
-
 	}
-	
 </style>

5f3cf5914528025f293cdea60fc8cd6f25d02bca
hul <9565244+hulhul@user.noreply.gitee.com>
2022-05-17 09:18:31 +0800

    数据

---
+++ pages/data/Item.vue
@@ -0,0 +1,30 @@
+<template>
+  <view class="demo-layout league-item">
+    <view class="img-box">
+      <image mode="aspectFit" :src="item.logo" alt="" />
+    </view>
+    <text>{{ item.name }}</text>
+  </view>
+</template>
+<script>
+export default {
+  name: "Item",
+  props: ["item"],
+};
+</script>
+<style lang="scss" scoped>
+.img-box {
+  width: 106rpx;
+  height: 106rpx;
+  uni-image {
+    height: 100%;
+  }
+}
+text {
+  margin-top: 12rpx;
+  display: block;
+  overflow: hidden; //溢出隐藏
+  white-space: nowrap; //禁止换行
+  text-overflow: ellipsis; //...
+}
+</style>
